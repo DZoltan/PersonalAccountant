94ab3539ce3f2feaafbc0bd471e2d3aaa05164c0
DZoltan <dobos.zoltan.esp@gmail.com>
2021-05-14 17:12:19 +0200

    test on ubuntu

--- src/main/java/accountant/Main.java
+++ src/main/java/accountant/Main.java
@@ -2,7 +2,9 @@ package accountant;
 
 import accountant.model.CashHandler;
 import javafx.application.Application;
-
+/**
+ * Personal Accountant
+ * @version 1.0.1*/
 public class Main {
 
     public static void main(String[] args) {
