8225a19c687e5cd57bce49371afc81b535701b79
DZoltan <dobos.zoltan.esp@gmail.com>
2021-05-16 19:58:47 +0200

    CashHandler documented.

--- src/main/java/accountant/model/CashHandler.java
+++ src/main/java/accountant/model/CashHandler.java
@@ -6,6 +6,7 @@ import org.checkerframework.checker.units.qual.C;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import org.tinylog.Logger;
 
 import java.sql.Date;
 import java.text.SimpleDateFormat;
@@ -31,37 +32,54 @@ public class CashHandler {
 
     ProfileHandler profileHandler = new ProfileHandler();
     CategoryHandler categoryHandler = new CategoryHandler();
-
+    /**
+     * Constructor of class. Create a table in DB, if not exists.
+     * */
     public CashHandler(){
         try{
             dao.createCashTable();
         }
         catch (Exception e){
-            System.out.println(e);
+            Logger.error("Error creating table" + e);
         }
     }
-
+    /**
+     * Store a new Transaction in Database.
+     * @param cash Object of {@link Cash} includes the data of transaction.
+     * */
     public void setNewTransaction(Cash cash){
         try{
             dao.insertCash(cash);
         }
         catch(Exception e){
-            System.out.println(e);
+            Logger.error("The application can't store this value in DataBase" + e);
         }
     }
 
-
+    /**
+     * Calls a method from DAO to return the list of Cash .
+     * @return Return the list of  Cash.
+     * */
     public List<Cash> getAllTransaction(){
         return  dao.getAllTransactions();
     }
 
+    /**
+     * Give an ID to new {@link Cash} objects
+     * @param listCash List of Cash
+     * @return if the list not empty max+ 1, otherwise 0
+     * */
     public int setCashId(List<Cash> listCash){
         if(listCash.size() == 0) return 0;
         else {
             return  listCash.stream().mapToInt(Cash::getCashId).max().orElseThrow() + 1;
         }
     }
-
+    /**
+     * Calls a method from DAO. The list will contain all {@link Cash} objects of user.
+     * @param profile_id Id of user.
+     * @return List of user's Cash.
+     * */
     public List<Cash> getOwnTransaction(int profile_id){
         return dao.getOwnTransactions(profile_id);
     }
@@ -74,7 +92,13 @@ public class CashHandler {
             return dao.getOutTransactions();
         }
     }
-
+    /**
+     * Parse a String to int, if the category is expense, number is negativev, otherwise positive.
+     * @param category category of {@link Cash}
+     * @param summaryText Given number in String from GUI.
+     * @return If the Category is expense : -{@param summaryText}, otherwise {@param summaryText}
+     * @exception NumberFormatException if the {@param summaryText} is NaN, throw this exception
+     * */
     public int ParseMoney(String summaryText, Category category){
          int money = Integer.parseInt(summaryText);
 
@@ -85,7 +109,10 @@ public class CashHandler {
             return 0-Math.abs(money);
         }
     }
-
+    /**
+     * Calculate the actual balance from the dates, which stored in Database, then store to profile.
+     * @param profile_id The id of user.
+     * */
     public void calculateBalance(int profile_id) {
         int balance = 0;
         for(int i = 0; i< getOwnTransaction(profile_id).size(); i++){
@@ -93,7 +120,12 @@ public class CashHandler {
         }
         profileHandler.updateBalance(profile_id, balance);
     }
-
+    /**
+     * Calculate the Summary of the expense in the past 7 days.
+     * This method ignores the incomes.
+     * @param total List of total cash of user.
+     * @return summary of expense in the past 7 days.
+     * */
     public int calculateWeeklyTotal(List<Cash> total)  {
 
         try {
@@ -111,11 +143,16 @@ public class CashHandler {
 
         }
         catch (DateTimeParseException e){
-            System.out.println(e);
+            Logger.error("Invalid Date " + e);
         }
         return 0;
     }
-
+    /**
+     * Calculate which was the most common category of the past 7 Days.
+     * The most common category is not equal with the highest expenditure.
+     * @param total List of user's Transaction
+     * @return Return whit the most common Category , if the list is empty or cannot be decided, this value is 0.
+     * */
     public int calculateWeeklyBest(List<Cash> total){
 
         if(total.size() == 0){
@@ -158,7 +195,12 @@ public class CashHandler {
         return category_id;
 
     }
-
+    /**
+     * Calculate the summary of the expense, which belongs to most common category.
+     * @param profile_id The id of the user.
+     * @param categoryName name of most common category -> {@code calculateWeeklyBest}
+     * @return Summary of the most common category's expense.
+     * */
     public int calculateWeeklyTotalOfBest(int profile_id, String categoryName){
 
         List<Cash> total = getOwnTransaction(profile_id);
@@ -174,8 +216,5 @@ public class CashHandler {
         return abs(weeklyTotalOfBest);
 
     }
-
-
-
 
 }
